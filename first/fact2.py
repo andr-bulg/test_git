368ca37de6977b51ad459a17484dd7e0289b8c9a
andr-bulg <127406892+andr-bulg@users.noreply.github.com>
2023-11-27 14:38:26 +0300

    Update fact2.py - 27-11-2023

--- first/fact2.py
+++ first/fact2.py
@@ -1,9 +1,11 @@
 def fact2(n):
   fact = 1
-  for i in range(2, n+1):
+  for i in range(2, n+1, 1):
     fact *= i
   return fact
 
 if __name__ == "__main__":
   print(fact2(5))
+  print(fact2(0))
+  
 
